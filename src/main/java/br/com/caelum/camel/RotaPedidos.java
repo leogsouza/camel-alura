9af125d1c3918677ac7855ad8093ded9728c225a
Leonardo Gonçalves <ldsz@gft.com>
2019-12-04 16:51:54 -0300

    Organizing sub routes

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -14,7 +14,15 @@ public class RotaPedidos {
 		context.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
-				from("file:pedidos?delay=5s&noop=true")
+                from("file:pedidos?delay=5s&noop=true")
+                .routeId("rota-pedidos")
+                .multicast()
+                    .to("direct:soap")
+                    .to("direct:http");
+
+
+				from("direct:http")
+                    .routeId("rota-http")
                     .setProperty("pedidoId", xpath("/pedido/id/text()"))
                     .setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
 					.split().xpath("/pedido/itens/item")
@@ -25,7 +33,12 @@ public class RotaPedidos {
 					.log("${id} - ${body}")
 					.setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
                     .setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}"))
-					.to("http4://localhost:8080/webservices/ebook/item");
+                .to("http4://localhost:8080/webservices/ebook/item");
+
+                from("direct:soap")
+                    .routeId("rota-soap")
+                    .setBody(constant("<envelope>teste</envelope>"))
+                .to("mock:soap");
 			}
 		});
 
